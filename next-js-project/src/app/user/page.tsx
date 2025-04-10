0f7f38057f0db801290ca9e0e5a7b9eef97cdc88
Divyansh Sharma <divyanshsharma4507@gmail.com>
2025-04-10 13:07:35 +0530

    Refactor User component to use async data fetching; remove outdated code and improve structure

--- next-js-project/src/app/user/page.tsx
+++ next-js-project/src/app/user/page.tsx
@@ -1,11 +1,78 @@
-//first method
+//first method => Bad way
 
-import React from 'react'
+// @ts-nocheck
 
-export default function User(){
+// "use client"
+// import React, { useEffect, useState } from 'react'
+
+// export default function User(){
+
+//     const [data, setData] = useState(null)
+//     const [loading, setLoading] = useState(false)
+//     const [error, setError] = useState(null)
+
+//     useEffect(()=>{
+//         setLoading(true)
+//         fetch('http://localhost:5000')
+//         .then(res => res.json())
+//         .then(data => {
+//             setData(data)
+//             setLoading(false)
+//         })
+//         .catch(err => {
+//             setError(err)
+//             setLoading(false)
+//         })
+//     }, [])
+
+
+//     return (
+//         <div className='user'>
+//             <h1>User Page</h1>
+//             {loading && <p>Loading...</p>}
+//             {error && <p>Error: {error.message}</p>}
+//             {data && (
+//                 <div>
+//                     <h2>User Details</h2>
+//                     <p>Name: {data.name}</p>
+//                     <p>Email: {data.email}</p>
+//                     <p>Contact: {data.contact}</p>
+//                     <h3>Address</h3>
+//                     <p>City: {data.address.city}</p>
+//                     <p>State: {data.address.state}</p>
+//                     <p>Country: {data.address.country}</p>
+//                     <p>Pincode: {data.address.pincode}</p>
+                    
+//                 </div>
+//             )}
+//             <button onClick={() => setData(null)}>Clear Data</button>
+//         </div>
+//     )
+// }
+
+
+
+
+//next-js way
+
+export default async function User() {
+    const res = await fetch('http://localhost:5000');
+    const data = await res.json();
+
     return (
         <div className='user'>
             <h1>User Page</h1>
+            <div>
+                <h2>User Details</h2>
+                <p>Name: {data.name}</p>
+                <p>Email: {data.email}</p>
+                <p>Contact: {data.contact}</p>
+                <h3>Address</h3>
+                <p>City: {data.address.city}</p>
+                <p>State: {data.address.state}</p>
+                <p>Country: {data.address.country}</p>
+                <p>Pincode: {data.address.pincode}</p>
+            </div>
         </div>
-    )
+    );
 }
