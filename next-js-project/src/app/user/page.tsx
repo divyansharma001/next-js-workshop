dc4cdb5c18df4a4e8ada12ff6f8d3ed2745e7fab
Divyansh Sharma <divyanshsharma4507@gmail.com>
2025-04-10 12:40:29 +0530

    Add User component with basic structure for user page

---
+++ next-js-project/src/app/user/page.tsx
@@ -0,0 +1,11 @@
+//first method
+
+import React from 'react'
+
+export default function User(){
+    return (
+        <div className='user'>
+            <h1>User Page</h1>
+        </div>
+    )
+}
